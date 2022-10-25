5906c48276928b2ca4261492e99366d87fb30850
istori1 <107304850+istori1@users.noreply.github.com>
2022-10-25 19:11:36 -0400

    Add files via upload

---
+++ sunshine-flatpak-installer.sh
@@ -0,0 +1,17 @@
+echo Sunshine Flatpak Installer
+echo The following operations will require sudo access.
+echo Granting temporary emulated mouse access
+sudo chown $USER /dev/uinput
+echo Setting up permanent emulated mouse access for the user $USER
+sudo usermod -a -G input $USER
+echo 'KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"' > 85-sunshine-input.rules
+sudo cp 85-sunshine-input.rules /etc/udev/rules.d/85-sunshine-input.rules
+rm 85-sunshine-input.rules
+wget https://github.com/LizardByte/Sunshine/releases/download/v0.14.1/sunshine.flatpak -O sunshine.flatpak
+sudo flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install --system -y ./sunshine.flatpak
+rm sunshine.flatpak
+echo You may now run Sunshine with: flatpak run dev.lizardbyte.sunshine
+echo If you need better performance run with sudo: "sudo -i PULSE_SERVER=unix:\$(pactl info | awk '/Server String/{print$3}') flatpak run dev.lizardbyte.sunshine"
+echo WARNING Running multiple instances will cause issues with ports.
+echo Once running access the Web UI at https://localhost:47990
---
+++ sunshine-flatpak-uninstaller.sh
@@ -0,0 +1,8 @@
+echo Sunshine Flatpak Uninstaller
+echo The following operations will require sudo access.
+echo Removing permanent emulated mouse access for the user $USER
+sudo gpasswd -d $USER input
+sudo rm /etc/udev/rules.d/85-sunshine-input.rules
+echo Removing Sunshine Flatpak
+sudo flatpak uninstall dev.lizardbyte.sunshine -y
+echo Sunshine Flatpak has been removed
